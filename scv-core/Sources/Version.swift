d9611c0d7abceacd59cb5adbad6be7ffc5865cf3
Karl Lew <karl@firepick.org>
2026-05-20 04:01:16 -0700

    Release candidate public let buildVersion = "26.06.0"

--- scv-core/Sources/Version.swift
+++ scv-core/Sources/Version.swift
@@ -6,10 +6,10 @@
 //
 
 /// Marketing version shown to users (software.year.month format)
-public let marketingVersion = "26.05"
+public let marketingVersion = "26.06"
 
 /// Build version for App Store (software.yearmonth.patch format)
-public let buildVersion = "26.05.6"
+public let buildVersion = "26.06.0"
 
 /// Serialization format version (bumped when Settings/Card format changes)
 /// Used to track backward compatibility and fixture versions
